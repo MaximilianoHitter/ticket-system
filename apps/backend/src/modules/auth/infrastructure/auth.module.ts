d1cdcea2e2e489a4b3366cb1ccdd86b47a904880
MaximilianoHitter <mr.vonhumbolt@gmail.com>
2026-09-05 20:30:57 -0300

    feat/shared-security
    se añaden los guards de roles y jwt
    
    #20 #21

--- apps/backend/src/modules/auth/infrastructure/auth.module.ts
+++ apps/backend/src/modules/auth/infrastructure/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/modules/users/infrastructure/users.module';
-import { HashingModule } from 'src/shared/hashing/infrastructure/hashing.module';
-import { TokenModule } from 'src/shared/token/infrastructure/token.module';
 import { AuthController } from './controllers/auth.controller';
 import { LoginUseCase } from '../application/login.use-case';
+import { HashingModule } from '@shared/hashing/infrastructure/hashing.module';
+import { TokenModule } from '@shared/token/infrastructure/token.module';
 
 @Module({
   imports: [UsersModule, HashingModule, TokenModule],
--- apps/backend/src/modules/users/infrastructure/users.module.ts
+++ apps/backend/src/modules/users/infrastructure/users.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { USER_REPOSITORY } from '../domain/interfaces/user-repository.interface';
 import { PrismaUserRepository } from './adapters/prisma-user.repository';
+import { UsersController } from './controllers/user.controller';
+import { SecurityModule } from '@shared/security/infrastructure/security.module';
 
 @Module({
+  imports: [SecurityModule],
   providers: [
     {
       provide: USER_REPOSITORY,
       useClass: PrismaUserRepository,
     },
   ],
+  controllers: [UsersController],
   exports: [USER_REPOSITORY],
 })
 export class UsersModule {}
